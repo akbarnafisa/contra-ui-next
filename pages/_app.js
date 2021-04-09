157a3255b38d3a2b2fb34636f0fa8403d5c29883
Akbar Nafisa Ja’far <akbar.nafisa@dana.id>
2021-04-09 19:35:17 +0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import { Provider } from '@contra-ui/core'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
